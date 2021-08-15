af3a424c972bd7bed35667f907d158f2f7e9f286
naruebes-dearw <dearv9@gmail.com>
2021-08-15 21:49:30 +0700

    add comment

--- 01-youtube-click-like-dislike.js
+++ 01-youtube-click-like-dislike.js
@@ -1,12 +1,19 @@
-// pressing Like and Dislike on youtube
+/* 
+  pushing Like and Dislike on youtube
+  1. open your favorite video on youtube.com
+  2. enter script in browser's console
+  3. enter  > push("like")
+            > push("dislike")
+*/
 
-function press(myClick) {
+function push(myClick) {
   const main = document.querySelectorAll('#top-level-buttons-computed')[1];
   const btn = main.querySelectorAll('button');
   
   const likeBtn = btn[0];
   const disBtn = btn[1];
   
+  // set bubbles:true - to click frome child node to parent node
   const click = new MouseEvent('click', { bubbles:true });
 
   if (myClick === "like") {
--- 02-autoclick-1to50.js
+++ 02-autoclick-1to50.js
@@ -1,39 +1,49 @@
+/* 
+  auto click 1 to 50
+  https://zzzscore.com/1to50/en/
+  1. enter script in browser's console to play the game
+  2. enter script again to back to first
+*/
+
 const retry = document.querySelector('.retry');
 const reset = document.querySelector('.resetBtn');
 const scoreText = document.querySelector('#score');
 const click = new MouseEvent('click', { bubbles:true });
 
-const delay = 110;
-let myLevel = 1;
+// initial setup
+const delay = 110; // delay between 1st and 2nd round
+let myLevel = 1; // 
 let myScore = (scoreText)? +scoreText.textContent : 0;
 
-// retry
+// retry - when in a result page
 if (retry) {
   retry.dispatchEvent(click);
   console.log("retry...");
 } 
-// restart
+// restart - when the game has already started
 else if (reset && myLevel !== myScore) {
   reset.dispatchEvent(click);
   console.log("reset...");
 }
 
 // can not run 1 to 50 at once
-// run 1 to 25
+// 1st round - run 1 to 25
 autoClick();
-// run 26 to 50
+// 2nd round - run 26 to 50
 setTimeout(() => {
   autoClick();
   console.log("done...");
 }, delay);
 
+// click boxes
 function autoClick() {
-  const boxs = document.querySelectorAll('.box');
+  const boxes = document.querySelectorAll('.box');
   const touchstart = new TouchEvent('touchstart');
   const touchend = new TouchEvent('touchend');
   
+  // click a box
   function clickBox() {
-    boxs.forEach(box => {
+    boxes.forEach(box => {
       const number = +box.nextSibling.textContent;
       if (number === myLevel) {
         box.parentNode.dispatchEvent(touchstart);
@@ -43,9 +53,13 @@ function autoClick() {
     });
   }
   
+  // set a round number
   const round = 25;
-  
+
   for (let i = 0; i < round; i++) {
     clickBox();
   }
 }
+
+// inspired by techCast
+// https://www.youtube.com/watch?v=O7nffbd6gqE&t=2s&ab_channel=Techcast
