1d04fa98493faaf2291954a972f4ea2339245441
naruebes-dearw <dearv9@gmail.com>
2021-08-16 13:15:23 +0700

    update popcat

--- 03-popcat-autoclick.js
+++ 03-popcat-autoclick.js
@@ -1,23 +1,50 @@
-const mouseDown = new KeyboardEvent('keydown', { key:'t' });
-const mouseUp = new KeyboardEvent('keyup', { key:'t' });
+/* 
+  popcat auto click
+  1. open website https://popcat.click/
+  2. enter script in browser's console
+  3. click on "POPCAT" text to run and stop
 
+  script run 1000 click per 30 seconds
+  ( API rate limit per IP address = 800 clicks per 30 seconds )
+*/
+
+const keyDown = new KeyboardEvent('keydown', { key:'t' });
+const keyup = new KeyboardEvent('keyup', { key:'t' });
+
+// initial setup
 let run = '';
 let status = false;
 
-// stop - click on the text "POPCAT" to stop
-document.addEventListener('click', () => {
-  status = status? false: true;
-  if (status) {
-    console.log('%c started... ', 'color:	#1a73e8; background:white')
-    run = setInterval((e) => {
-      document.dispatchEvent(mouseDown);
-      document.dispatchEvent(mouseUp);
-    }, 30);
-  } else {
-    clearInterval(run);
-    console.log('%c stop ', 'color:red; background:white');
-    document.cookie = 'pop_count=0';
+// clear bot status
+document.cookie.split(';').map(cookie => {
+  const isBot = cookie.match(/bot=.+/g);
+  if ( isBot ) {
+    document.cookie = 'bot='; // clear bot status
+    console.log('clear bot and refresh.');
+    location.reload(); // refresh website
   }
+})
+
+// click on the text "POPCAT" to run and stop.
+document.addEventListener('click', () => {
+  status = status? false: true; // toggle status
+  status? start() : stop();
 });
+
+// start
+function start() {
+  console.log('%c started... ', 'color:	#1a73e8; background:white')
+  run = setInterval((e) => {
+    document.dispatchEvent(keyDown);
+    document.dispatchEvent(keyup);
+  }, 30); // click every 30 ms
+}
+
+// stop
+function stop() {
+  clearInterval(run); // stop running
+  console.log('%c stop ', 'color:red; background:white');
+  // document.cookie = 'pop_count=0'; // reset count
+}
 
 console.log('%c   Click on "POPCAT" text to run and stop.   ', 'color:green; background:white')
